375f48abc1dae15c3ef9a1a736e20f2b860a9619
Valeriia Khylenko <l.khylenko@gmail.com>
2020-06-26 13:33:47 +0300

    Test Automation Page Object

---
+++ PageObject/Lab2_pages/Pages/UpdatePassword.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium.Support.UI;
+
+namespace Lab2_pages.Pages
+{
+    public class UpdatePassword
+    {
+        private IWebDriver _driver;
+
+        [FindsBy(How = How.XPath, Using = "/html/body/div[7]/div[1]/div[5]/div[2]/a")]
+        public IWebElement changePassBtn;
+        [FindsBy(How = How.XPath, Using = "//*[@id='userUpdatePasswordCurrent']")]
+        public IWebElement userUpdatePasswordCurrent;
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='userPasswordNew']")]
+        public IWebElement userPasswordNew;
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='userPasswordConfirm']")]
+        public IWebElement userPasswordConfirm;
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='modal-content']/div[6]/div/a")]
+        public IWebElement confBtn;
+        
+        [FindsBy(How = How.XPath, Using = "/html/body/div[12]/div/div[5]/a[1]")]
+        public IWebElement notification;
+
+        public UpdatePassword(IWebDriver driver)
+        {
+            _driver = driver;
+            _driver.Manage().Window.Maximize();
+            PageFactory.InitElements(driver, this);
+        }
+
+        public String GetCurrntUrl() => _driver.Url;
+
+        public UpdatePassword ChangePassword(String mypass, String newpass)
+        {
+            new WebDriverWait(_driver, TimeSpan.FromSeconds(100)).Until(ExpectedConditions.ElementIsVisible(By.XPath(("/html/body/div[12]/div/div[5]/a[1]"))));
+            notification.Click();
+            System.Threading.Thread.Sleep(300);            
+            new WebDriverWait(_driver, TimeSpan.FromSeconds(100)).Until(ExpectedConditions.ElementIsVisible(By.XPath("/html/body/div[7]/div[1]/div[5]/div[2]/a")));
+            changePassBtn.Click();
+            new WebDriverWait(_driver, TimeSpan.FromSeconds(100)).Until(ExpectedConditions.ElementIsVisible(By.XPath("//*[@id='userUpdatePasswordCurrent']")));
+            userUpdatePasswordCurrent.SendKeys(mypass);
+            userPasswordNew.SendKeys(newpass);
+            userPasswordConfirm.SendKeys(newpass);
+
+            confBtn.Click();
+
+            return new UpdatePassword(_driver);
+
+        }
+    }
+}
